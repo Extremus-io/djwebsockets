e73755538199068453ef74825dfd6bcaec496e3a
kittuov <kittuov@gmail.com>
2015-09-14 08:13:23 +0530

    updated description

--- setup.py
+++ setup.py
@@ -9,7 +9,11 @@ setup(
     license='BCD',
     author='kittu',
     author_email='kittuov@gmail.com',
-    description='A django wrapper for websockets. for more info visit "http://github.com/extremus-io/djwebsockets"',
+    description='A django wrapper for websockets. for more info visit my GitHub page',
+    long_description='''
+The app is almost production ready. It lacks good documentation. But you can get started with tutorial at
+http://github.com/extremus-io/djwebsockets
+    ''',
     classifiers=[
         'Development Status :: 6 - Mature',
         'Programming Language :: Python :: 3'
